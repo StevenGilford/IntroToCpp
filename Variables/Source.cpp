14945b090d0ab37273b0051d9a607f9e1feb453a
StevenGilford <Steven.P.Gilford@gmail.com>
2017-08-15 09:32:49 -0500

    Variables Exercise

---
+++ Variables/Source.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+void main()
+{
+	//You can not print to the console unless the problem specifies.
+
+	//1. Select suitable variable types and names for the following examples
+	//a) The average height of students in your class
+	float avgHeight = 5.5f;
+	//b) The tax rate for incomes over $65000
+	int taxRate = 'a' >= 65000;
+	//c) The tax rate for incomes below $5000
+	int taxRate2 = 'b' >= 5000;
+	//d) The total cost of all the games on your Steam library
+	float stmLibrary = 47.08;
+
+	//2. Select suitable data types to hold the following values :
+	//a) 121
+	int ctMeter = 121;
+	//b) 9.4
+	float ftHeight = 9.4f;
+	//c) r
+	char footHeight = 'r';
+	//d) 100000
+	int milliMeter = 100000;
+	//e) False
+	bool False;
+
+	//3. Create a program that swaps two numbers. Ask the user for 2 numbers in the console window
+	//and store the result in variables called “a” and “b” print the result to screen.Write code 
+	//that swaps the values for these variables, then print the variables “a” and “b” again.
+	//You should comment your code.
+
+	int a;
+	int b;
+	a = b; //this makes the two values equal.
+	b = a; //this swaps the values.
+	int c = a + b;
+	
+		
+
+
+	//4. Ask the user for 5 numbers and output the average of these numbers.	
+	int f;
+	int g;
+	int h;
+	int i;
+	int j;  //these variabl;es are all separate numbers
+	(f * g * h * i * j) / (f + g + h + i + j) //this is the equation of averages with the variable numbers inside
+	
+		
+	//5. Redo question 3 but allows the user to input the values to be swapped and provide information
+	//in the console proving the data was swapped.
+
+
+
+
+}
