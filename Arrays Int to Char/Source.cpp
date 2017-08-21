0f86c3c39c66422ae319f08d9d6c42d65b160684
StevenGilford <steven.p.gilford@gmail.com>
2017-08-21 17:55:20 -0500

    First Attempt
    Prototype form

---
+++ Arrays Int to Char/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+ int main()
+{
+	//The problem to be solved is to declare an array of integers.
+	//That array will have 255 elements.
+	//For every item in that array print out its character equivalent.
+	//int ints[];
+	//int ints[255];
+	char b = 97;
+	int Muse[255]; 
+	for (int Muse = 0; Muse <= 255; Muse++)
+	{
+		Muse = b + 65;
+		if (Muse < 256)
+		{
+			
+		}
+		std::cout << Muse;
+	}
+	
+}
