2081b05a21125f007d8a43dfd5a25c9c65df9707
StevenGilford <steven.p.gilford@gmail.com>
2017-08-22 15:43:55 -0500

    2 out of 7 Questions
    Assignment partially completed

--- Creating Arrays/Source.cpp
+++ Creating Arrays/Source.cpp
@@ -2,30 +2,55 @@
 //sepcified).
 //a. heights of students
 
+float studentHeight [];
+
 //b. age of parents
+
+int parentalAges [];
 
 //c. true of false questions
 
+bool questionList[];
+
 //d. letters of the alphabet
+
+char Alphabet [26];
 
 //2. What is the size in memory of the following arrays
 //a. 
 float heights[5];
 
+//The memory size is 20 bytes.
+
 //b. 
 int ages[10];
 
+//The memory size is 40 bytes.
+
 //c. 
 char letters[26];
+
+//the memory size is 26 bytes.
+
 //3. Declare an array with the values 1 - 10;
+
+int 
 
 //4. Using the array created in problem 3 print the 5th index.
 
+
+
 //5. Using a loop, populate an array of size 10 with the values 10 - 1.
+
+
 
 //6. Create a an array of size five then ask the user to input five 
 //number into the array. Display the largest and smallest numbers 
 //to the console.
 
+
+
 //7. Create a program that creates a small 3x3 array of integers.Use a nested for 
 //loop to initialise the numbers 1 - 9. Output the numbers in a grid format.
+
+
