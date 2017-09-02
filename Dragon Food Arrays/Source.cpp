9f2733f29f559d77b2b001c1a8f2cf5448e54484
StevenGilford <Steven.P.Gilford@gmail.com>
2017-09-02 11:21:35 -0500

    space error
    correction

--- Dragon Food Arrays/Source.cpp
+++ Dragon Food Arrays/Source.cpp
@@ -12,8 +12,8 @@ int main()
 	//	2 dimensional 3 x 7 array, where each row represents a different dragon and each column
 	//	represents a different day of the week.Then it should create a report that includes the
 	//	following information :
-  //-Average amount of food eaten per day by all the dragons together
-	//	- Average amount of food eaten per day by any one dragon
+    //-Average amount of food eaten per day by all the dragons together
+	//	- Average amount of food eaten per da y by any one dragon
 	//	- The greatest amount of food eaten during one day and which dragon that was
 	//	- The least amount of food eaten during one day and which dragon that was
 
