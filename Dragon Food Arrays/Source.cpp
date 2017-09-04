2e3ffe7e91c06d9e61ade3dda0fb172a30f6c6b8
StevenGilford <Steven.P.Gilford@gmail.com>
2017-09-03 20:07:00 -0500

    Soectacular Concept Failure
    Lack of Time

--- Dragon Food Arrays/Source.cpp
+++ Dragon Food Arrays/Source.cpp
@@ -11,22 +11,62 @@ int main()
 	//	each day during a typical week.Write a program that stores this information in a
 	//	2 dimensional 3 x 7 array, where each row represents a different dragon and each column
 	//	represents a different day of the week.Then it should create a report that includes the
-	//	following information :
-    //-Average amount of food eaten per day by all the dragons together
-	//	- Average amount of food eaten per da y by any one dragon
+	//	following information:
+	//  - Average amount of food eaten per day by all the dragons together
+	//	- Average amount of food eaten per day by any one dragon
 	//	- The greatest amount of food eaten during one day and which dragon that was
 	//	- The least amount of food eaten during one day and which dragon that was
 
 	int Bob;
 	int Cat;
 	int Scott;
-	int dragonFood[3][7] = { {10, 1, 11, 4, 0, 20, 5},
-							 {5, 17, 1, 2, 6, 12, 8},
-							 {3, 8, 9, 15, 9, 4, 9} };
-	Bob = { 10, 1, 11, 4, 0, 20, 5 };
+	int bobAverageDailyFood;
+	int catAverageDailyFood;
+	int scottAverageDailyFood;
+	int averageFoodDailyTogether;
+	int dragonFood[3][7] = { { 10, 1, 11, 4, 0, 20, 5 },
+	{ 5, 17, 1, 2, 6, 12, 8 },
+	{ 3, 8, 9, 15, 9, 4, 9 } };
+	Bob = { [0][0],[0][1],[0][2],[0][3],[0][4],[0][5],[0][6],[0][7] };
 	Cat = { 5, 17, 1, 2, 6, 12, 8 };
 	Scott = { 3, 8, 9, 15, 9, 4, 9 };
 
+	for (int i = 0; i < 7; i++)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			averageFoodDailyTogether = Bob + Cat + Scott;
+		}
+		std::cout << averageFoodDailyTogether;
 
+	}
+
+	bobAverageDailyFood = dragonFood{ [0],[1] } +{[0], [2]} +{[0], [3]} +{[0], [4]} +{[0], [5]} +{[0], [6]} +{[0], [7]} / 7;
+	catAverageDailyFood = dragonFood{ [1],[1] } +{[1], [2]} +{[1], [3]} +{[1], [4]} +{[1], [5]} +{[1], [6]} +{[1], [7]} / 7;
+	scottAverageDailyFood = dragonFood{ [2],[1] } +{[2], [2]} +{[2], [3]} +{[2], [4]} +{[2], [5]} +{[2], [6]} +{[2], [7]} / 7;
+
+	int a = 0;
+	int b = 0;
+	int i;
+	for (int i = 0; i < 7; i++)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			if (i == 0)
+			{
+				a = dragonFood[i];
+				b = dragonFood[i];
+			}
+			else
+			{
+				if (dragonFood[i] < a)
+					a = dragonFood[i];
+				if (dragonFood[i] > b)
+					b = dragonFood[i];
+			}
+		}
+	}
+	std::cout << "Least amount of food eaten is from" << "Bob and Cat" << a << std::endl;
+	std::cout << "Greatest amount of food eaten is from " << "Bob" << b << std::endl;
 
 }
