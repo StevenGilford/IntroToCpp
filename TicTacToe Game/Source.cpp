eaa7704a846ab7921a0f556d48853e757c0df864
StevenGilford <steven.p.gilford@gmail.com>
2017-09-06 12:16:23 -0500

    basic setting setup
    of game space

--- TicTacToe Game/Source.cpp
+++ TicTacToe Game/Source.cpp
@@ -1,1 +1,35 @@
 #include <iostream>
+
+
+void main()
+{
+	int worldGrid[3][3];
+	int roadways;
+	int towers;
+	char player = 'X';
+	char player2 = 'O';
+	int gameSpace;
+    std::cout << "  0    1   2\n";
+    std::cout << "     |   |   \n";
+    std::cout << "  ===========\n";
+    std::cout << "  1  |   |   \n";
+    std::cout << "  ===========\n";
+    std::cout << "  2  |   |   \n";
+    std::cout << "choose a space\n";
+    std::cin >> gameSpace;
+switch (gameSpace)
+{
+case 1: 
+case 2: 
+case 3: 
+case 4: 
+case 5: 
+case 6: 
+case 7: 
+case 8: 
+case 9: 
+default: std::cout << "this is outside the grid, please try again.";
+
+}
+system("pause");
+}
